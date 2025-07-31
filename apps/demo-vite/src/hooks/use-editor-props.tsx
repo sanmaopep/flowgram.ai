881a3889775a6a7af723fecc506a5477956a1d5e
sanmaopep <maoyiwei.ebay@bytedance.com>
2025-07-31 21:43:51 +0800

    feat: remove useless files

--- apps/demo-vite/src/hooks/use-editor-props.tsx
+++ apps/demo-vite/src/hooks/use-editor-props.tsx
@@ -14,30 +14,9 @@ import {
   Field,
   useNodeRender,
 } from '@flowgram.ai/free-layout-editor';
-import { CodeEditor } from '@flowgram.ai/form-materials';
 
 import { nodeRegistries } from '../node-registries';
 import { initialData } from '../initial-data';
-
-const defaultCode = `// Here, you can retrieve input variables from the node using 'params' and output results using 'ret'.
-// 'params' has been correctly injected into the environment.
-// Here's an example of getting the value of the parameter named 'input' from the node input:
-// const input = params.input;
-// Here's an example of outputting a 'ret' object containing multiple data types:
-// const ret = { "name": 'Xiaoming', "hobbies": ["Reading", "Traveling"] };
-
-async function main({ params }) {
-  // Build the output object
-  const ret = {
-      "key0": params.input + params.input, // Concatenate the input parameter 'input' twice
-      "key1": ["hello", "world"], // Output an array
-      "key2": { // Output an Object
-          "key21": "hi"
-      },
-  };
-
-  return ret;
-}`;
 
 export const useEditorProps = () =>
   useMemo<FreeLayoutProps>(
