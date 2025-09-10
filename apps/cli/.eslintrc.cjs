7292ae703a58d7c34dc61c3b9ddc70830f42468a
sanmaopep <maoyiwei.ebay@bytedance.com>
2025-09-10 20:11:39 +0800

    fix: cli lint

--- apps/cli/.eslintrc.cjs
+++ apps/cli/.eslintrc.cjs
@@ -12,9 +12,4 @@ module.exports = defineConfig({
     'no-console': 'off',
     'react/prop-types': 'off',
   },
-  settings: {
-    react: {
-      version: 'detect', // 自动检测 React 版本
-    },
-  },
 });
